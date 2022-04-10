52fb6b79b6631ff524209f4058c9eb9851e2d255
pavanleton <pavankumarkpkp@gmail.com>
2022-04-10 18:12:46 +0530

    update: query response handling in home page

--- components/CharactersList/index.tsx
+++ components/CharactersList/index.tsx
@@ -2,22 +2,44 @@ import React, {useState} from "react";
 
 // COMPONENTS
 import ListView from "components/CharactersList/ListView";
+import Pagination from "components/Pagination";
+import {LoadingText} from "components/CharactersList/styles";
 
 // GQL
-import {Character, GetCharactersQuery, useGetCharactersQuery} from "graphql/graphql-gen";
+import {Character, useGetCharactersQuery, Info} from "graphql/graphql-gen";
 
 const CharactersList: React.FC = () => {
-    const [characters, setCharacters] = useState<Character[]>([])
+    const [currentPage, setCurrentPage] = useState<number>(1)
 
-    const {loading} = useGetCharactersQuery({
-        variables: {page: 1},
-        onCompleted: (data: GetCharactersQuery) => {
-            setCharacters(data.characters?.results as Character[] || [])
-        }
+    const {loading, refetch, data } = useGetCharactersQuery({
+        variables: {page: currentPage},
+        onError: () => {
+            alert('Something went wrong, please try again after some times')
+        },
+        nextFetchPolicy: 'cache-and-network',
+        fetchPolicy: 'cache-and-network'
     })
 
+    const updatePagination = async (pageNum: number) => {
+        try {
+            setCurrentPage(pageNum);
+            await refetch({page: currentPage})
+            return
+        } catch (error) {
+            alert('Something went wrong, please try again after some times')
+        }
+    }
+
+    if(loading) {
+        return  <LoadingText>
+            Loading...
+        </LoadingText>
+    }
+
     return <>
-        <ListView list={characters} />
+        <Pagination {...(data?.characters?.info as Info)} currentPage={currentPage} handlePageChange={updatePagination}  />
+        <ListView list={(data?.characters?.results as Character[]) || []} />
+        <Pagination {...(data?.characters?.info as Info)} currentPage={currentPage} handlePageChange={updatePagination}  />
     </>
 }
 
