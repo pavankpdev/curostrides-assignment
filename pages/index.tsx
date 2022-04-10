71b16b4240b979a8050e148b781f2dd4059437c2
pavanleton <pavankumarkpkp@gmail.com>
2022-04-10 16:06:11 +0530

    feat: display rick and morty characters table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 
+// COMPONENTS
+import CharactersList from "components/CharactersList";
+
 const Home: NextPage = () => {
+
   return (
     <div>
-
+      <CharactersList />
     </div>
   )
 }
