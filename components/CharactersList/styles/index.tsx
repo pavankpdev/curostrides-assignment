37f92ecb6a4c261b8a14aaa4f75070befca545c3
pavanleton <pavankumarkpkp@gmail.com>
2022-04-10 16:05:16 +0530

    update: add list styles

---
+++ components/CharactersList/styles/index.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const List = styled.table`
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+`;
+
+export const ListHead = styled.thead`
+  background-color: #8bc34a;
+`;
+
+export const ListBody = styled.tbody`
+  background-color: #eeeeee;
+`;
+
+export const ListRow = styled.tr`
+  
+`;
+
+export const ListHeadItem = styled.th`
+  padding: 1rem;
+  text-align: center;
+`;
+
+export const ListItem = styled.td`
+  padding: .5rem;
+  text-align: center;
+`;
+
+
+
