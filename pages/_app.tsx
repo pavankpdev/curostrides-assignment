bba26e9235846c56ed8ed5970846f550d0290b09
pavanleton <pavankumarkpkp@gmail.com>
2022-04-10 18:14:24 +0530

    update: add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Head from "next/head";
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
@@ -9,9 +10,16 @@ const client = new ApolloClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <>
+          <Head>
+              <title>
+                  Rick and morty
+              </title>
+          </Head>
+          <ApolloProvider client={client}>
+              <Component {...pageProps} />
+          </ApolloProvider>
+      </>
   )
 }
 
